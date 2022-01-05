b201141511cc74581668b82f5119c0054441cfa8
Anton Michna <antonmichna@gmail.com>
2022-01-05 13:51:28 -0600

    Initial Commit

---
+++ Veggie Eater Two/Veggie_Eater_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Veggie_Eater_TwoApp.swift
+//  Veggie Eater Two
+//
+//  Created by Anton Michna on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Veggie_Eater_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
